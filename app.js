6b53434828034dd9166487d4fd2fc6684ee89bea
unknown <17njtan@stu.edu.cn>
2018-09-24 20:59:14 +0800

    宁静修改的页面

--- app.js
+++ app.js
@@ -1,40 +1,84 @@
 //app.js
 App({
-
-  onShow:function(){
+  onLaunch: function () {
+   let temp={};
+  wx.getStorage({
+    key: 'key',
+    success: function (res) {
+      console.log("success");
+      console.log(res.data);
+      // 异步接口在success回调才能拿到返回值
+     temp= res.data;
+      //this.num = this.globalData.dataList.num
+    },
+    fail: function () {
+      console.log('读取key发生错误')
+    }
+    
+  })
+  this.globalData.dataList = temp;
+ // console.log(this.globalData);
+ 
+  },
+  onShow: function () {
     var that = this;
     wx.getStorage({
-      key: 'key',
-      success: function (res) {
+        key: 'all',
+        success: function (res) {
         console.log("success")
         console.log(res.data)
         that.globalData.datalist = res.data;
 
-        if (that.globalData.datalist.num == null) {
-          that.globalData.datalist.num = 0;
+        if (that.globalData.datalist.situation == null) {
+          that.globalData.datalist.situation = 1;
         }
       },
       fail: function () {
         console.log('读取key发生错误')
       }
     })
+    wx.getStorage({
+      key: 'count',
+      success: function (res) {
+        console.log("success")
+        console.log(res.data)
+        that.globalData.num = res.data;
+        if (that.globalData.num == null) {
+          that.globalData.datalist.situation = 0;
+        }
+      },
+    })
   },
-  onHide:function(){
+  onHide: function () {
     wx.setStorage({
-      key: "key",
+      key: "all",
       data: this.globalData.datalist,
       success: function () {
         console.log('写入value1成功')
       },
+
+      fail: function () {
+        console.log('写入value1发生错误')
+      }
+
+    })
+    wx.setStorage({
+      key: 'count',
+      data: this.globalData.num,
+      success: function () {
+        console.log('写入value1成功')
+      },
+
       fail: function () {
         console.log('写入value1发生错误')
       }
     })
-  }, 
+  },
   globalData: {
-    datalist:{},
-    num:0,
+    datalist: {},
+    num: 0,
     unNum:0
+	
   },
 
 })
--- pages/add/add.js
+++ pages/add/add.js
@@ -1,66 +1,154 @@
-// pages/add/add.js
+// pages/detail/detail.js
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    name: null,
+    infor: null,
+    array: ['', '生活', '工作', '学习', '其他'],
+    index: 0,
+    situation: 0,
+    label: null,
+    num: 0,
+    dataList: {},
+    temp:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.data.dataList = app.globalData.dataList;//对局部变量初始化为全局变量
+    this.data.num = app.globalData.num;
+    console.log(this.data.num);
+    console.log(this.data.dataList);
+  },
+
+  getName(event) {
+    this.setData({
+      name: event.detail
+    })
+  },
+
+  getInfor(event) {
+    this.setData({
+      infor: event.detail
+    })
+  },
+
+  bindPickerChange: function (event) {
+    let array = this.data.array;
+    this.setData({
+      label: array[event.detail.value]
+    })
+  },
+
+  addTask() {
+    if (this.data.name){
+      wx.showLoading({
+        title: '创建中'
+      });
+      //console.log("success");
+      //把数据存为对象
+      var temp ={
+        "name": this.data.name,
+        "infor": this.data.infor,
+        "label": this.data.array[this.data.index],
+        "situation": 1
+      }
+      //记录下标
+      var index="task"+this.data.num;
+      console.log(temp);
+      // console.log(temp.keyname);
+      // this.dataList=app.globalData.dataList;
+      // console.log(this.dataList);
+      //增加任务
+      this.data.dataList[index] = temp;
+      // console.log(app.globalData.dataList[index]);
+      //num自增
+      this.data.num++;
+      //全局的datalist同步局部的dataList
+      app.globalData.dataList = this.data.dataList;
+      app.globalData.num = this.data.num;
+      // console.log(app.globalData.dataList);
+      /* this.setData({
+         taskname:null,
+         taskdetail:null,
+         index:0
+       })*/
+      wx.hideLoading();
+      wx.showToast({
+        title: '创建成功',
+      })
+      wx.navigateBack({
+        delta: 1
+      })
+    }
+    else{//判断是否任务名为空
+      wx.showModal({
+        title: '',
+        content: '任务名不能为空哦！',
+        confirmText: "确定",
+        cancelText: "取消",
+
+      });
+    }
+    
+    
+
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
