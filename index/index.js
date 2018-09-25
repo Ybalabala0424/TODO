c3e12f2c2a1923f8749bbcbac1183d424fae531a
Ybalabala0424 <39643766+Ybalabala0424@users.noreply.github.com>
2018-09-25 10:07:09 +0800

    Add files via upload

--- index/index.js
+++ index/index.js
@@ -4,25 +4,32 @@ var util = require('../../utils/util.js');
 Page({
   data: {
     isShowHistory: 0,
+    unDo:[],
+    dataList:{}
   },
   onLoad: function () {
     var time = util.formatTime(new Date());
+    this.data.dataList = app.globalData.dataList; 
     this.setData({
       time: time
     })
-    console.log(app.globalData.dataList)
   },
   onShow:function(){
     var unNum = 0
+    var unDo = []
     for (var i in app.globalData.dataList) {
       var si = app.globalData.dataList[i].situation;
       if (si) {
+        unDo.push(app.globalData.dataList[i])
         unNum++
       }
     }
     this.setData({
-      unNum:unNum
+      unNum:unNum,
+      unDo:unDo
     })
+    console.log(app.globalData.dataList)
+    console.log(this.data.dataList)
   },
   btn: function () {
     var that = this;
