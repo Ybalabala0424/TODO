02116977140c36b1addc8f98c117cbd048bcb1e8
Ybalabala0424 <39643766+Ybalabala0424@users.noreply.github.com>
2018-09-26 16:22:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,11 @@
 //app.js
 App({
   onLoad: function () {
-    this.globalData.dataList = wx.getStorageSync('key')/*({
-      key: 'key',
-      success: function (res) { 
-        console.log('成功')
-      },
-    })*/
-      this.globalData.num = wx.getStorageSync('count')/*({
-        key: 'count',
-        success: function (res) { 
-          console.log('成功')
-        },
-      })*/
+    this.globalData.dataList = wx.getStorageSync('key')
   },
   onHide:function(){
-    wx.setStorageSync('key', this.globalData.dataList)/*({
-      key: "key",
-      data: this.globalData.dataList,
-      success: function () {
-        console.log('写入value1成功')
-      },
-      fail: function () {
-        console.log('写入value1发生错误')
-      }
-    },*/
-    wx.setStorageSync('count', this.globalData.num)/*{
-      key:'count',
-      data: this.globalData.num,
-      success: function () {
-        console.log('写入num成功')
-      },
-      fail: function () {
-        console.log('写入num发生错误')
-      }
-    })*/
+    wx.setStorageSync('key', this.globalData.dataList)
+    wx.setStorageSync('count', this.globalData.num)
   }, 
   globalData: {
     dataList:{},
