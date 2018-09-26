5177779a21e251b08511f5bc1265d91bfcc4892d
Ybalabala0424 <39643766+Ybalabala0424@users.noreply.github.com>
2018-09-26 18:00:06 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ App({
   onLoad: function () {
     this.globalData.dataList = wx.getStorageSync('key')
   },
-  onHide:function(){
-    wx.setStorageSync('key', this.globalData.dataList)
-    wx.setStorageSync('count', this.globalData.num)
-  }, 
   globalData: {
     dataList:{},
     num:0,
